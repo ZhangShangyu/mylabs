6f018e87fbfd1ca1245e61b093c1bf0de66f0551
ZhangShangyu <shangyu_zhang@outlook.com>
2019-04-02 00:32:49 +0800

    annotation test

---
+++ java/annotation/src/main/java/TestMain.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+@TestAnnotation(count = 123456)
+public class TestMain {
+
+    // javac TestAnnotation.java TestMain.java
+    // javap -v TestAnnotation
+    // java -Dsun.misc.ProxyGenerator.saveGeneratedFiles=true TestMain
+
+    public static void main(String[] args) throws IOException {
+        TestAnnotation annotation = TestMain.class.getAnnotation(TestAnnotation.class);
+        System.out.println(annotation.count());
+        System.in.read();
+    }
+
+}
